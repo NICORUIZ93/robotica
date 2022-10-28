80eb091cd0a134434a36c6338781f1611c5871a2
Nicolas Ruiz <nicolasenriqueruiz93@hotmail.com>
2022-10-28 11:53:34 -0500

    ok mejorado

--- deteccion.py
+++ deteccion.py
@@ -3,13 +3,13 @@ import cv2
 classes = {0: "pelota", 1: "cancha"}
 clasificacion = cv2.CascadeClassifier("./modelos/classifier/cascade.xml")
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     pelota = clasificacion.detectMultiScale(
-        gray, scaleFactor=1.1, minNeighbors=1)
+        gray, scaleFactor=1.1, minNeighbors=3)
 
     if ret == False:
         break
